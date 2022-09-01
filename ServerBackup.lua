d9f2e2413d55bea58c47ffeedfa9ef375bef18cc
Xatmo <42683376+Xatmo980@users.noreply.github.com>
2022-08-31 19:39:24 -0700

    Add backup by dated folder option

--- ServerBackup.lua
+++ ServerBackup.lua
@@ -1,7 +1,9 @@
 local Backup = {}
-local ResetTime = 86400     -- reset every 86400 seconds (24 hours) of real time.
+local ResetTime = 86400     -- Exterior cells reset every 86400 seconds (24 hours) of real time.
+local SeperateFolder = true -- [Untested on Linux]Put each day in seperate dated folder instead of overwite(Takes up more space)
 local BackupPlayers = true  -- "true" to backup all players .json files
 local BackupWorld = true    -- "true" to backup world.json
+
 
 customEventHooks.registerHandler("OnServerPostInit", function(eventStatus, pid)
         tes3mp.LogMessage(3, "Backups Online!!")
@@ -29,23 +31,41 @@ local getPlayers = function(directory)
    return t
 end
 
+Backup.Date = function()
+    Date = os.date("%m-%d-%Y(%I-%p)")
+  return Date
+end
+
 Backup.All = function()
       if BackupPlayers == true then
          local directory = tes3mp.GetDataPath() .. "/player/"
          local Files = getPlayers(directory)
          local players = "/custom/backups/players/"
+         if SeperateFolder == true then
+                  os.execute("mkdir " .. tes3mp.GetDataPath() .. "\\custom\\backups\\players\\" .. Backup.Date())
+         end
                for _,fileName in pairs(Files) do
                    local NoFileExtension = fileName:split(".")
                    local fileName = NoFileExtension[1]
+                   
                    local PlayerData = jsonInterface.load("/player/" .. fileName .. ".json")
-                   jsonInterface.save(players .. fileName .. ".json", PlayerData)
+                   if SeperateFolder == true then
+                      jsonInterface.save(players .. Backup.Date() .. "/" .. fileName .. ".json", PlayerData)
+                   else
+                      jsonInterface.save(players .. fileName .. ".json", PlayerData)
+                   end
                end
          tes3mp.LogMessage(3, "(" .. #Files .. ")" .. "Players Backed Up!!")
       end
       if BackupWorld == true then
          local world = "/custom/backups/world/"
          local WorldData = jsonInterface.load("/world/world.json")
-         jsonInterface.save(world .. "world.json", WorldData)
+               if SeperateFolder == true then
+                  os.execute("mkdir " .. tes3mp.GetDataPath() .. "\\custom\\backups\\world\\" .. Backup.Date())
+                  jsonInterface.save(world .. Backup.Date() .. "/" .. "world.json", WorldData)
+               else
+                  jsonInterface.save(world .. "world.json", WorldData)
+               end
          tes3mp.LogMessage(3, "World Backed Up!!")
       end
 
